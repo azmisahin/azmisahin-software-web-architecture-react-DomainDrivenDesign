bde846fe3736d0b3bf8fcfc5b417d3a244fd2dae
Azmi ŞAHİN <azmisahin@outlook.com>
2017-09-10 01:28:38 +0300

    CI Fix

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ class App extends Component {
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
               </li>
             </ul>
           </div>
