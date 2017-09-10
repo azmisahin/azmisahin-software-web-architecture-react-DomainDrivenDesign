05dc599673ed7fa27811d23cf66ccd6a422d3a6c
Azmi ŞAHİN <azmisahin@outlook.com>
2017-09-10 16:31:55 +0300

    Heroku Build Image and Any error fix

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ class App extends Component {
       <div>
 
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">React</a>
+          <a className="navbar-brand" href="#Home">React</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
             aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -30,7 +30,7 @@ class App extends Component {
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="#Home">Home <span className="sr-only">(current)</span></a>
               </li>
             </ul>
           </div>
