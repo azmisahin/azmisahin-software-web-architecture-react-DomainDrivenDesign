8cf38db8964dbab462569251160803ebe4d351f7
Azmi ŞAHİN <azmisahin@outlook.com>
2017-09-14 22:38:30 +0300

    Timer Component File Add

---
+++ src/components/timer/index.js
@@ -0,0 +1,68 @@
+/**
+ * ====================================================================================================
+ * Name    : Timer Script
+ * File    : timer.js
+ * Version : 0.0.1
+ * ====================================================================================================
+ */
+
+// Require
+import React, { Component } from 'react';
+
+// Referance
+
+/**
+ * Timer Component
+ */
+class Timer extends React.Component {
+
+    // Constructor
+    constructor(props) {
+
+        // Base
+        super(props);
+
+        // Property
+        this.state = { secondsElapsed: 0 };
+    }
+
+    // Timer Tick 
+    tick() {
+
+        // Set State
+        this.setState((prevState) => ({
+
+            // Set State from old state
+            secondsElapsed: prevState.secondsElapsed + 1
+        }));
+    }
+
+    // Component Did Mount
+    componentDidMount() {
+
+        // Timer Start
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    // Component Un Mount
+    componentWillUnmount() {
+
+        // Timer Clear
+        clearInterval(this.interval);
+    }
+
+    // Render Element
+    render() {
+        return (
+            <div className="alert alert-secondary">
+                Seconds Elapsed:
+                <span className="badge badge-primary">
+                    {this.state.secondsElapsed}
+                </span>
+            </div>
+        );
+    }
+}
+
+// Export Module
+export default Timer
